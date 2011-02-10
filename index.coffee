39c3519d2df5dca944fa5df15e79b3f95b7c3874
drewalex@gmail.com <=>
2011-02-10 13:27:32 +0000

    now in coffeescript

---
+++ index.coffee
@@ -0,0 +1,53 @@
+_.templateSettings = 
+    interpolate : /\{\{(.+?)\}\}/g
+
+makeVars = (vars) ->
+  for name, value of vars
+    if k.isNumeric value
+      vars[name] = value
+    else
+      vars[name] = "scope." + value.replace /(?:\.{2}([\w]+)\b)/g, (a,b) -> "[#{b}]" #using the bracket notation
+  vars  
+
+
+interpolate = (str, rawVars) ->
+  _.template str, makeVars rawVars
+
+parse = window.parse = (txt) ->
+  functions = []
+  txt = txt.split "\n"
+  for line, index in txt
+    line = k.trimLeft line
+    code = ""
+    code += "function(scope) {"
+    if k.startsWith(line, "string") || k.startsWith(line, '"')
+      code += "scope.so = \"" + k.s line, line.indexOf(" ") + 1 + '"'
+    else if k.startsWith line, "`"
+      code += s line, line.indexOf " " + 1
+    else 
+      line = k.trimRight line
+      line = line.replace /\s+/, " "
+      line = line.split " "
+      if line[0] is "set" or line[0] is "="
+        if not line[2] then line[2] = "so"
+        code += interpolate "{{varName}} = {{varValue}}", varName: line[1], varValue: line[2] 
+        
+      else if line[0] is "goto"
+        code += interpolate "scope.pc = {{varValue}}", varValue: line[1]
+    code += "}"
+    functions.push(code)
+ 
+  compiled = """
+    scope = {}
+    functions = [#{functions.join(",\n")}]
+    scope.pc = 0
+    for (var j=0; j<100l j++) {
+      if (scope.pc >= functions.length) {
+        break;  
+      }
+      functions[scope.pc](scope);
+      scope.pc ++
+    }
+  """
+  console.log compiled
+  eval compiled
---
+++ index.js
@@ -0,0 +1,61 @@
+(function() {
+  var interpolate, makeVars, parse;
+  _.templateSettings = {
+    interpolate: /\{\{(.+?)\}\}/g
+  };
+  makeVars = function(vars) {
+    var name, value;
+    for (name in vars) {
+      value = vars[name];
+      if (k.isNumeric(value)) {
+        vars[name] = value;
+      } else {
+        vars[name] = "scope." + value.replace(/(?:\.{2}([\w]+)\b)/g, function(a, b) {
+          return "[" + b + "]";
+        });
+      }
+    }
+    return vars;
+  };
+  interpolate = function(str, rawVars) {
+    return _.template(str, makeVars(rawVars));
+  };
+  parse = window.parse = function(txt) {
+    var code, compiled, functions, index, line, _len;
+    functions = [];
+    txt = txt.split("\n");
+    for (index = 0, _len = txt.length; index < _len; index++) {
+      line = txt[index];
+      line = k.trimLeft(line);
+      code = "";
+      code += "function(scope) {";
+      if (k.startsWith(line, "string") || k.startsWith(line, '"')) {
+        code += "scope.so = \"" + k.s(line, line.indexOf(" ") + 1 + '"');
+      } else if (k.startsWith(line, "`")) {
+        code += s(line, line.indexOf(" " + 1));
+      } else {
+        line = k.trimRight(line);
+        line = line.replace(/\s+/, " ");
+        line = line.split(" ");
+        if (line[0] === "set" || line[0] === "=") {
+          if (!line[2]) {
+            line[2] = "so";
+          }
+          code += interpolate("{{varName}} = {{varValue}}", {
+            varName: line[1],
+            varValue: line[2]
+          });
+        } else if (line[0] === "goto") {
+          code += interpolate("scope.pc = {{varValue}}", {
+            varValue: line[1]
+          });
+        }
+      }
+      code += "}";
+      functions.push(code);
+    }
+    compiled = "scope = {}\nfunctions = [" + (functions.join(",\n")) + "]\nscope.pc = 0\nfor (var j=0; j<100l j++) {\n  if (scope.pc >= functions.length) {\n    break;  \n  }\n  functions[scope.pc](scope);\n  scope.pc ++\n}";
+    console.log(compiled);
+    return eval(compiled);
+  };
+}).call(this);
