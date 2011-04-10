df23996d122b046e35d78073df2dfda00aaf8545
drewlesueur <drewalex@gmail.com>
2011-04-10 17:48:39 +0000

    label

--- index.coffee
+++ index.coffee
@@ -1,27 +1,39 @@
 scope = {}
 functions = []
 compile = (code) ->
+  functions = []
+  scope = {}
   code = code.split "\n"  
   for line in code
     console.log line 
     line = line.split " "
     func = line[0]
+    if func.length is 0 then continue
     args = _.s line, 1
     functions.push """
-      funciton() {
-        #{func}("#{args.join('", "')}");
+      function() {
+        #{func}(\"#{args.join("\", \"")}");
       }
     """
   
   """ 
+    function set(name, val) {
+      scope[name] = val
+    }
+    function label(name) {
+      set(name, scope.pc);
+    }
     var scope = #{JSON.stringify(scope)};
-    functions = [#{functions.join(',\n')}]
+    functions = [\n#{functions.join(',\n')}]
     scope.pc = 0
     scope.last_pc = 0
+    scope.set_pc = -1
     for (var j=0; j<10000; j++) {
       if (scope.pc >= functions.length || scope.__close__ == true) {
+        console.log(scope);
         break;  
       }
+      console.log(scope.pc)
       functions[scope.pc](scope);
       scope.second_last_pc = scope.last_pc
       scope.last_pc = scope.pc
@@ -29,7 +41,7 @@ compile = (code) ->
         scope.pc = scope.set_pc
         scope.set_pc = -1
       }
-      scope.pc ++
+      scope.pc++
     }
   """
 
@@ -38,8 +50,16 @@ compile = (code) ->
 
 $(document).ready () ->
   $('#code').persistValue()
-  $("#compile").click () ->
+  compileCode = () ->
     code = $('#code').val()
     compiled = compile code 
     $('#output1').val compiled
+    eval compiled
+  $("#compile").click compileCode
+
+  $(document.body).bind "keydown", (e) ->
+    if e.keyCode is 116 #f5
+      e.preventDefault()
+      compileCode()
+
 
--- index.js
+++ index.js
@@ -4,24 +4,38 @@
   functions = [];
   compile = function(code) {
     var args, func, line, _i, _len;
+    functions = [];
+    scope = {};
     code = code.split("\n");
     for (_i = 0, _len = code.length; _i < _len; _i++) {
       line = code[_i];
       console.log(line);
       line = line.split(" ");
       func = line[0];
+      if (func.length === 0) {
+        continue;
+      }
       args = _.s(line, 1);
-      functions.push("funciton() {\n  " + func + "(\"" + (args.join('", "')) + "\");\n}");
+      functions.push("function() {\n  " + func + "(\"" + (args.join("\", \"")) + "\");\n}");
     }
-    return " \nvar scope = " + (JSON.stringify(scope)) + ";\nfunctions = [" + (functions.join(',\n')) + "]\nscope.pc = 0\nscope.last_pc = 0\nfor (var j=0; j<10000; j++) {\n  if (scope.pc >= functions.length || scope.__close__ == true) {\n    break;  \n  }\n  functions[scope.pc](scope);\n  scope.second_last_pc = scope.last_pc\n  scope.last_pc = scope.pc\n  if (scope.set_pc != -1) {\n    scope.pc = scope.set_pc\n    scope.set_pc = -1\n  }\n  scope.pc ++\n}";
+    return " \nfunction set(name, val) {\n  scope[name] = val\n}\nfunction label(name) {\n  set(name, scope.pc);\n}\nvar scope = " + (JSON.stringify(scope)) + ";\nfunctions = [\n" + (functions.join(',\n')) + "]\nscope.pc = 0\nscope.last_pc = 0\nscope.set_pc = -1\nfor (var j=0; j<10000; j++) {\n  if (scope.pc >= functions.length || scope.__close__ == true) {\n    console.log(scope);\n    break;  \n  }\n  console.log(scope.pc)\n  functions[scope.pc](scope);\n  scope.second_last_pc = scope.last_pc\n  scope.last_pc = scope.pc\n  if (scope.set_pc != -1) {\n    scope.pc = scope.set_pc\n    scope.set_pc = -1\n  }\n  scope.pc++\n}";
   };
   $(document).ready(function() {
+    var compileCode;
     $('#code').persistValue();
-    return $("#compile").click(function() {
+    compileCode = function() {
       var code, compiled;
       code = $('#code').val();
       compiled = compile(code);
-      return $('#output1').val(compiled);
+      $('#output1').val(compiled);
+      return eval(compiled);
+    };
+    $("#compile").click(compileCode);
+    return $(document.body).bind("keydown", function(e) {
+      if (e.keyCode === 116) {
+        e.preventDefault();
+        return compileCode();
+      }
     });
   });
 }).call(this);
